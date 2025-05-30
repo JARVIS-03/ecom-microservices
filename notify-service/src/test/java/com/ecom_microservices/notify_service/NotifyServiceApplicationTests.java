1043bbc13dc0b9dc7b8d62cc60f08df95143b4ac
kalai <kalaiyarasansky33@gmail.com>
2025-05-30 14:42:00 +0530

    E-com Structure Updated

---
+++ notify-service/src/test/java/com/ecom_microservices/notify_service/NotifyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom_microservices.notify_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
