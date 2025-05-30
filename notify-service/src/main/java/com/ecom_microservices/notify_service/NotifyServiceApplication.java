2f54e106cd132481440dd36e8026d987d86f46f4
venkatesh <nagapayay@virtusa.com>
2025-05-30 15:37:34 +0530

    Eureka server added for all services

--- notify-service/src/main/java/com/ecom_microservices/notify_service/NotifyServiceApplication.java
+++ notify-service/src/main/java/com/ecom_microservices/notify_service/NotifyServiceApplication.java
@@ -2,8 +2,10 @@ package com.ecom_microservices.notify_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NotifyServiceApplication {
 
 	public static void main(String[] args) {
