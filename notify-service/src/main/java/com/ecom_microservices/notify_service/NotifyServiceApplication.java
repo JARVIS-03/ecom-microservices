d6277d60564f3f6324388a5430722e3fd47cffdd
Prasad Ugale <prasadugale123@gmail.com>
2025-06-02 20:10:10 +0530

    notify-service: send email retry logic

--- notify-service/src/main/java/com/ecom_microservices/notify_service/NotifyServiceApplication.java
+++ notify-service/src/main/java/com/ecom_microservices/notify_service/NotifyServiceApplication.java
@@ -3,12 +3,15 @@ package com.ecom_microservices.notify_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableRetry
+@EnableScheduling
 public class NotifyServiceApplication {
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotifyServiceApplication.class, args);
--- notify-service/src/main/java/com/ecom_microservices/notify_service/repository/NotificationRepository.java
+++ notify-service/src/main/java/com/ecom_microservices/notify_service/repository/NotificationRepository.java
@@ -3,7 +3,10 @@ package com.ecom_microservices.notify_service.repository;
 import com.ecom_microservices.notify_service.enums.NotificationStatus;
 import com.ecom_microservices.notify_service.model.Notification;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -16,4 +19,12 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     List<Notification> findByStatus(NotificationStatus status);
     List<Notification> findByRecipient(String recipient);
     List<Notification> findByCreatedTimestampBetween(LocalDateTime startDate, LocalDateTime endDate);
+    
+    @Query(value = "SELECT n FROM Notification n WHERE n.status = :status " +
+            "ORDER BY CASE n.priority " +
+            "WHEN 'HIGH' THEN 1 " +
+            "WHEN 'MEDIUM' THEN 2 " +
+            "WHEN 'LOW' THEN 3 END," +
+            "n.createdTimestamp ASC")
+    List<Notification> findNotificationByStatusOrderByPriority(@Param("status") NotificationStatus status, Pageable pageable);
 }
