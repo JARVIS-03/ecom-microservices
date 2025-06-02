eac1df5e5533f7ed51a94afc1f164eca2f27b3a0
nive1712 <nivesundaramoorthy@gmail.com>
2025-06-02 18:15:55 +0530

    Code updated

--- notify-service/src/main/java/com/ecom_microservices/notify_service/service/NotificationService.java
+++ notify-service/src/main/java/com/ecom_microservices/notify_service/service/NotificationService.java
@@ -5,6 +5,8 @@ import com.ecom_microservices.notify_service.dto.NotificationResponseDTO;
 import com.ecom_microservices.notify_service.enums.NotificationStatus;
 import com.ecom_microservices.notify_service.model.Notification;
 import com.ecom_microservices.notify_service.repository.NotificationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @Service
 public class NotificationService {
+    private static final Logger logger = LoggerFactory.getLogger(NotificationService.class);
 
     private final NotificationRepository repository;
 
@@ -20,6 +23,7 @@ public class NotificationService {
     }
 
     public NotificationResponseDTO createNotification(NotificationRequestDTO requestDTO) {
+        logger.info("Creating notification for recipient: {}", requestDTO.getRecipient());
         Notification notification = Notification.builder()
                 .recipient(requestDTO.getRecipient())
                 .messageContent(requestDTO.getMessageContent())
@@ -30,7 +34,7 @@ public class NotificationService {
                 .build();
 
         Notification saved = repository.save(notification);
-
+        logger.debug("Notification saved with ID: {}", saved.getId());
         return new NotificationResponseDTO(
                 saved.getId(),
                 saved.getRecipient(),
@@ -44,14 +48,23 @@ public class NotificationService {
     }
 
     public List<Notification> getNotificationsByStatus(NotificationStatus status) {
-        return repository.findByStatus(status);
+        logger.info("Fetching notifications with status: {}", status);
+        List<Notification> notifications =  repository.findByStatus(status);
+        logger.debug("Fetched {} notifications with status {}", notifications.size(), status);
+        return notifications;
     }
 
     public List<Notification> getNotificationsByRecipient(String recipient) {
-        return repository.findByRecipient(recipient);
+        logger.info("Fetching notifications for recipient: {}", recipient);
+        List<Notification> notifications = repository.findByRecipient(recipient);
+        logger.debug("Fetched {} notifications for recipient {}", notifications.size(), recipient);
+        return notifications;
     }
     public List<Notification> getNotificationsByDateRange(LocalDateTime startDate, LocalDateTime endDate) {
-        return repository.findByCreatedTimestampBetween(startDate, endDate);
+        logger.info("Fetching notifications between {} and {}", startDate, endDate);
+        List<Notification> notifications =  repository.findByCreatedTimestampBetween(startDate, endDate);
+        logger.debug("Fetched {} notifications in date range", notifications.size());
+        return notifications;
     }
 
 
