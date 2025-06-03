5d1d888a24225dfec207e39e362efd641a9c1fc1
VenkateshPro <venkateshnaga579@gamil.com>
2025-06-03 13:41:26 +0530

    Dto added

--- product-service/src/main/java/com/ecom_microservices/product_service/dto/ProductRequest.java
+++ product-service/src/main/java/com/ecom_microservices/product_service/dto/ProductRequest.java
@@ -1,5 +1,30 @@
 package com.ecom_microservices.product_service.dto;
 
+
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductRequest {
-//    just for test
+    @NotBlank(message = "Product ID is mandatory")
+    private String productId;
+
+    @NotBlank(message = "Product name is mandatory")
+    private String name;
+
+    @Positive(message = "Price must be greater than zero")
+    private double price;
+
+    @NotBlank(message = "Category is mandatory")
+    private String category;
+
+    @NotNull(message = "Availability must be specified")
+    private Boolean available;
 }
