381182f94c35081494d6425967c313e24df231e5
Nimish Lohumi <91203793+Ace-09@users.noreply.github.com>
2025-06-03 12:57:59 +0530

    Update Product.java Model

--- product-service/src/main/java/com/ecom_microservices/product_service/model/Product.java
+++ product-service/src/main/java/com/ecom_microservices/product_service/model/Product.java
@@ -1,4 +1,35 @@
 package com.ecom_microservices.product_service.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Product ID is mandatory")
+    @Column(unique = true, nullable = false)
+    private String productId;
+
+    @NotBlank(message = "Product name is mandatory")
+    private String name;
+
+    @Positive(message = "Price must be greater than zero")
+    private double price;
+
+    @NotBlank(message = "Category is mandatory")
+    private String category;
+
+    private boolean available;
 }
