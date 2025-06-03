0b2de5e1640645cc25bd768d6a85fd560b289d0e
Arjun Patidar <94211987+arjunpatidar23@users.noreply.github.com>
2025-06-03 13:05:56 +0530

    Update ProductController.java

--- product-service/src/main/java/com/ecom_microservices/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/ecom_microservices/product_service/controller/ProductController.java
@@ -1,4 +1,33 @@
 package com.ecom_microservices.product_service.controller;
 
+
+import com.product.product_service.dto.ProductRequest;
+import com.product.product_service.dto.ProductResponse;
+import com.product.product_service.model.Product;
+import com.product.product_service.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
+ @GetMapping("/{productId}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable String productId) {
+        ProductResponse response = productService.getProductById(productId);
+        return ResponseEntity.ok(response);
+    }
 }
