3642d9474ac160ffd0b89863b5f87155fc5208e3
Mavilla Vamsi Krishna <vamsikrishnamavilla@gmail.com>
2025-06-21 22:40:09 +0530

    logs changed to manual

--- payment-service/src/main/java/com/ecom/payment/paymentservice/retry/RetryLogger.java
+++ payment-service/src/main/java/com/ecom/payment/paymentservice/retry/RetryLogger.java
@@ -1,14 +1,18 @@
 package com.ecom.payment.paymentservice.retry;
 
+import com.ecom.payment.paymentservice.service.NotificationServiceClient;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.retry.RetryCallback;
 import org.springframework.retry.RetryContext;
 import org.springframework.retry.RetryListener;
 import org.springframework.stereotype.Component;
 
-@Slf4j
+//@Slf4j
 @Component("retryLogger")
 public class RetryLogger implements RetryListener {
+    private static final Logger log = LoggerFactory.getLogger(NotificationServiceClient.class);
     @Override
     public <T, E extends Throwable> boolean open(RetryContext context, RetryCallback<T, E> callback) {
         return true;
